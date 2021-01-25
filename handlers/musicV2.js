47a3b71c693dcf8621d40dbb6c914775f4d1969e
Tet <ic3dplasma@gmail.com>
2021-01-24 23:49:07 -0800

    nya

--- handlers/musicV2.js
+++ handlers/musicV2.js
@@ -116,7 +116,7 @@ class MusicHandler {
 			}
 			let stream = ytdl(songLink, {
 				highWaterMark: 1024 * 1024 * 1024,
-				quality: "audio",
+				quality: "lowestaudio",
 				requestOptions: {
 					headers: {
 						cookie: this.bot.ytCookies[Math.round(Math.random() * 100) % this.bot.ytCookies.length],
