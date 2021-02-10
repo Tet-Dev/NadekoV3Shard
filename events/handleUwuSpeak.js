fcbd83f8f713345c10556c46e11f43299929ea07
Tet <ic3dplasma@gmail.com>
2021-02-10 11:53:21 -0800

    Quotes work now!

--- events/handleUwuSpeak.js
+++ events/handleUwuSpeak.js
@@ -55,186 +55,194 @@ const httpRegex = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/
 module.exports = new DiscordEvent({
 	name: "messageCreate",
 	run: async (bot, msg) => {
-		
-		if (msg.attachments.length) return;
-		if (uwuSpeakCache.has(msg.channel.id)) {
-			(async () => {
+		try {
+
+
+			if (msg.attachments.length) return;
+			if (uwuSpeakCache.has(msg.channel.id)) {
+				(async () => {
+					let data = await bot.SQLHandler.getChannel(msg.channel.id);
+
+					uwuSpeakCache.set(msg.channel.id, data.uwuspeak ? true : false);
+				})();
+			} else {
 				let data = await bot.SQLHandler.getChannel(msg.channel.id);
-
 				uwuSpeakCache.set(msg.channel.id, data.uwuspeak ? true : false);
-			})();
-		} else {
-			let data = await bot.SQLHandler.getChannel(msg.channel.id);
-			uwuSpeakCache.set(msg.channel.id, data.uwuspeak ? true : false);
-		}
-
-		let res;
-
-
-		res = uwuSpeakCache.get(msg.channel.id) || hasUwuSpeak(msg.member);
-		if (!res) return;
-		if (msg.author.id === bot.user.id) return;
-		if (msg.author.discriminator === "0000") return;
-		let webHooks = await msg.channel.getWebhooks();
-		let hooks = [];
-		for (let i = 0; i < webHooks.length; i++) {
-			let hook = webHooks[i];
-			if (hook.type == 1 && hook.name === "Dazai UwUSpeakHook") {
-				hooks.push({ id: hook.id, token: hook.token });
 			}
-		}
-		if (hooks.length < 5) {
-			for (let i = 0; i < (5 - hooks.length); i++) {
-				let webhookInfo = await msg.channel.createWebhook({ name: "Dazai UwUSpeakHook", avatar: bot.user.avatar }, "For UwuSpeak");
-				hooks.push({ id: webhookInfo.id, token: webhookInfo.token });
-				// hooks.push("https:\/\/discordapp.com/api/webhooks/" + webhookInfo.id + "/" + webhookInfo.token);
-			}
-		}
-		let reply = msg.messageReference && msg.messageReference.messageID;
-		if (msg.messageReference && msg.messageReference.messageID){
-			msg.content = `https://discord.com/${msg.messageReference.guildID}/${msg.messageReference.channelID}/${msg.messageReference.messageID}` + msg.content;
-		}
-		let data = findLinks(msg.content);
-		let linkedmsgs;
-		let extraembeds = [];
-		if (data.links.length) {
-			
-			linkedmsgs = await Promise.all(data.links.map(async (x) => {
-				let refm = await bot.getMessage(x.chanid, x.msgid);
-				if (!refm) return false;
-				
-				if (refm.embeds.length && data.links.length + extraembeds.length + refm.embeds.length < 9) {
-					extraembeds = extraembeds.concat(refm.embeds.map(x => {
-						x.title = x.title ? "Ref Embed: " + x.title : "Referenced Embed";
-						return x;
-					}));
+
+			let res;
+
+
+			res = uwuSpeakCache.get(msg.channel.id) || hasUwuSpeak(msg.member);
+			if (!res) return;
+			if (msg.author.id === bot.user.id) return;
+			if (msg.author.discriminator === "0000") return;
+			let webHooks = await msg.channel.getWebhooks();
+			let hooks = [];
+			for (let i = 0; i < webHooks.length; i++) {
+				let hook = webHooks[i];
+				if (hook.type == 1 && hook.name === "Dazai UwUSpeakHook") {
+					hooks.push({ id: hook.id, token: hook.token });
 				}
-				let imgobj;
-				if (refm.attachments.length == 1) {
-					imgobj = {
-						url: refm.attachments[0].url
+			}
+			if (hooks.length < 5) {
+				for (let i = 0; i < (5 - hooks.length); i++) {
+					let webhookInfo = await msg.channel.createWebhook({ name: "Dazai UwUSpeakHook", avatar: bot.user.avatar }, "For UwuSpeak");
+					hooks.push({ id: webhookInfo.id, token: webhookInfo.token });
+					// hooks.push("https:\/\/discordapp.com/api/webhooks/" + webhookInfo.id + "/" + webhookInfo.token);
+				}
+			}
+			let reply = msg.messageReference && msg.messageReference.messageID;
+			if (msg.messageReference && msg.messageReference.messageID) {
+				msg.content = `https://discord.com/channels/${msg.messageReference.guildID}/${msg.messageReference.channelID}/${msg.messageReference.messageID} ` + msg.content;
+				// console.log(msg.content);
+			}
+			let data = findLinks(msg.content);
+			let linkedmsgs;
+			let extraembeds = [];
+			if (data.links.length) {
+
+				linkedmsgs = await Promise.all(data.links.map(async (x) => {
+					let refm = await bot.getMessage(x.chanid, x.msgid);
+					if (!refm) return false;
+					if (refm.author.bot){
+						if (refm.embeds.length && data.links.length + extraembeds.length + refm.embeds.length < 9) {
+							extraembeds = extraembeds.concat(refm.embeds.map(x => {
+								x.title = x.title ? "Ref Embed: " + x.title : "Referenced Embed";
+								return x;
+							}));
+						}
+					}
+
+					let imgobj;
+					if (refm.attachments.length == 1) {
+						imgobj = {
+							url: refm.attachments[0].url
+						};
+					}
+					if (reply)
+						data.msg += `||${refm.author.mention}||`;
+					return {
+						author: {
+							name: refm.author.username || "Unknown",
+							// url: 
+							icon_url: (refm.author ? refm.author : bot.user).dynamicAvatarURL("png", 256),
+						},
+						description: (refm.content || "Unknown") + ` [[Jump]](https://discord.com/channels/${x.guildid}/${x.chanid}/${x.msgid})`,
+						image: imgobj,
+
 					};
-				}
-				if (reply)
-					msg.content += `||${refm.author.mention}||`;
-				return {
-					author: {
-						name: refm.author.username || "Unknown",
-						// url: 
-						icon_url: (refm.author ? refm.author : bot.user).dynamicAvatarURL("png", 256),
-					},
-					description: (refm.content || "Unknown") + ` [[Jump]](https://discord.com/channels/${x.guildid}/${x.chanid}/${x.msgid})`,
-					image: imgobj,
+				}));
+				linkedmsgs = linkedmsgs.filter(x => x).concat(extraembeds);
 
-				};
-			}));
-			linkedmsgs = linkedmsgs.filter(x => x).concat(extraembeds);
-			
-			linkedmsgs.push({
-				"color": null,
-				"footer": {
-					"text": "This message was modified from an original user message by Dazai."
-				}
-			});
-		}
-		msg.content = data.msg;
-
-
-
-		const links = msg.content.match(httpRegex);
-		if (links) msg.content = msg.content.replace(httpRegex, "{蟹$§$蟹}");
-		// eslint-disable-next-line no-control-regex
-		msg.content.replace(/[^\x00-\x7F]+/g,"");
-		msg.content = msg.content.replace(/l/g, "w");
-		msg.content = msg.content.replace(/r/g, "w");
-		msg.content = msg.content.replace(/(?:r|l)/g, "w");
-		msg.content = msg.content.replace(/(?:R|L)/g, "W");
-		msg.content = msg.content.split("NA").join("å");
-		msg.content = msg.content.split("NE").join("é");
-		msg.content = msg.content.split("NI").join("î");
-		msg.content = msg.content.split("NO").join("ø");
-		msg.content = msg.content.split("NU").join("ü");
-		msg.content = msg.content.replace(/n([aeiou])/g, "ny$1");
-		msg.content = msg.content.replace(/N([aeiou])/g, "Ny$1");
-		msg.content = msg.content.replace(/ove/g, "uv");
-		msg.content = msg.content.replace(/<@\!/g, "<@\$∞");
-		msg.content = msg.content.replace(/\!+/g, " " + faces[Math.floor(Math.random() * faces.length)] + " ");
-		msg.content = msg.content.replace(/<@\$∞/g, "<@\!");
-		msg.content = msg.content.split("å").join("NYA");
-		msg.content = msg.content.split("é").join("NYE");
-		msg.content = msg.content.split("î").join("NYI");
-		msg.content = msg.content.split("ø").join("NYO");
-		msg.content = msg.content.split("ü").join("NYU");
-		if (!msg.content) return;
-		
-		let Selhook = hooks[(msg.author.id % 10) % hooks.length];
-		if (links) {
-			for (let i = 0; i < links.length; i++) {
-				msg.content = msg.content.replace("{蟹$§$蟹}", links[i]);
+				linkedmsgs.push({
+					"color": null,
+					"footer": {
+						"text": "This message was modified from an original user message by Dazai."
+					}
+				});
 			}
-		}
-		msg.delete();
-		let tempitem = {
-			allowedMentions:{
-				everyone:false,
-				roles:false,
-				users:false,
-			},
-			auth: true,
-			content: msg.content,
-			embeds: linkedmsgs,
-			"username": msg.member.nick || msg.author.username,
-			avatarURL: msg && msg.author.avatarURL ? msg.author.avatarURL.replace(".jpg", ".png") : bot.user.avatarURL,
-			wait: true,
-		};
-		
-		let t2 = {
-			allowedMentions:{
-				everyone:false,
-				roles:false,
-				users:false,
-			},
-			auth: true,
-			content: "...",
-			embeds: linkedmsgs,
-			"username": msg.member.nick || msg.author.username,
-			avatarURL: msg && msg.author.avatarURL ? msg.author.avatarURL.replace(".jpg", ".png") : bot.user.avatarURL,
-			wait: true,
-		};
+			msg.content = data.msg;
 
-		// t2.content = "Loading..."
-		let webMsg = await bot.executeWebhook(Selhook.id, Selhook.token, msg.mentions.length ? t2 : tempitem).catch(er => console.trace(er));
 
-		if (msg.mentions.length) {
-			await axios(
-				{
-					method: "patch",
-					url: "https://discord.com/api/v8/webhooks/" + Selhook.id + "/" + Selhook.token + "/messages/" + webMsg.id,
-					headers: {
-						"Content-Type": "application/json"
-					},
-					data: JSON.stringify(tempitem)
+
+			const links = msg.content.match(httpRegex);
+			if (links) msg.content = msg.content.replace(httpRegex, "{蟹$§$蟹}");
+			// eslint-disable-next-line no-control-regex
+			msg.content.replace(/[^\x00-\x7F]+/g, "");
+			msg.content = msg.content.replace(/l/g, "w");
+			msg.content = msg.content.replace(/r/g, "w");
+			msg.content = msg.content.replace(/(?:r|l)/g, "w");
+			msg.content = msg.content.replace(/(?:R|L)/g, "W");
+			msg.content = msg.content.split("NA").join("å");
+			msg.content = msg.content.split("NE").join("é");
+			msg.content = msg.content.split("NI").join("î");
+			msg.content = msg.content.split("NO").join("ø");
+			msg.content = msg.content.split("NU").join("ü");
+			msg.content = msg.content.replace(/n([aeiou])/g, "ny$1");
+			msg.content = msg.content.replace(/N([aeiou])/g, "Ny$1");
+			msg.content = msg.content.replace(/ove/g, "uv");
+			msg.content = msg.content.replace(/<@\!/g, "<@\$∞");
+			msg.content = msg.content.replace(/\!+/g, " " + faces[Math.floor(Math.random() * faces.length)] + " ");
+			msg.content = msg.content.replace(/<@\$∞/g, "<@\!");
+			msg.content = msg.content.split("å").join("NYA");
+			msg.content = msg.content.split("é").join("NYE");
+			msg.content = msg.content.split("î").join("NYI");
+			msg.content = msg.content.split("ø").join("NYO");
+			msg.content = msg.content.split("ü").join("NYU");
+			if (!msg.content) return;
+
+			let Selhook = hooks[(msg.author.id % 10) % hooks.length];
+			if (links) {
+				for (let i = 0; i < links.length; i++) {
+					msg.content = msg.content.replace("{蟹$§$蟹}", links[i]);
 				}
+			}
+			msg.delete();
+			let tempitem = {
+				allowedMentions: {
+					everyone: false,
+					roles: false,
+					users: false,
+				},
+				auth: true,
+				content: msg.content,
+				embeds: linkedmsgs,
+				"username": msg.member.nick || msg.author.username,
+				avatarURL: msg && msg.author.avatarURL ? msg.author.avatarURL.replace(".jpg", ".png") : bot.user.avatarURL,
+				wait: true,
+			};
+
+			let t2 = {
+				allowedMentions: {
+					everyone: false,
+					roles: false,
+					users: false,
+				},
+				auth: true,
+				content: "...",
+				embeds: linkedmsgs,
+				"username": msg.member.nick || msg.author.username,
+				avatarURL: msg && msg.author.avatarURL ? msg.author.avatarURL.replace(".jpg", ".png") : bot.user.avatarURL,
+				wait: true,
+			};
+
+			// t2.content = "Loading..."
+			let webMsg = await bot.executeWebhook(Selhook.id, Selhook.token, msg.mentions.length ? t2 : tempitem).catch(er => console.trace(er));
+
+			if (msg.mentions.length) {
+				await axios(
+					{
+						method: "patch",
+						url: "https://discord.com/api/v8/webhooks/" + Selhook.id + "/" + Selhook.token + "/messages/" + webMsg.id,
+						headers: {
+							"Content-Type": "application/json"
+						},
+						data: JSON.stringify(tempitem)
+					}
+				);
+			}
+
+			// bot.webhookEditMessage(bot, webMsg.webhookID, Selhook.token, webMsg.id, tempitem);
+			// webMsg.webhookEdit(Selhook.token, tempitem);
+			const reactionListener = new ReactionHandler.continuousReactionStream(
+				webMsg,
+				(userID) => (userID.id ? userID.id : userID) === msg.author.id,
+				false,
+				{ maxMatches: 1000, time: 60000 * 10 }
 			);
+			reactionListener.on("reacted", async (event) => {
+				if (event.emoji.name.startsWith("❌")) {
+					bot.deleteMessage(webMsg.channel.id, webMsg.id);
+					webMsg.delete();
+
+				}
+
+
+			});
+		} catch (er) {
+			console.trace(er);
 		}
-
-		// bot.webhookEditMessage(bot, webMsg.webhookID, Selhook.token, webMsg.id, tempitem);
-		// webMsg.webhookEdit(Selhook.token, tempitem);
-		const reactionListener = new ReactionHandler.continuousReactionStream(
-			webMsg,
-			(userID) => (userID.id ? userID.id : userID) === msg.author.id,
-			false,
-			{ maxMatches: 1000, time: 60000 * 10 }
-		);
-		reactionListener.on("reacted", async (event) => {
-			if (event.emoji.name.startsWith("❌")) {
-				bot.deleteMessage(webMsg.channel.id, webMsg.id);
-				webMsg.delete();
-
-			}
-
-
-		});
 	}
 });
 
